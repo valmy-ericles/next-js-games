16fcf2de08761b0d5a06953ec13681e334325e66
Valmy Ericles Nunes Machado <valmyericlesnunesmachado@MacBook-Air-de-Valmy.local>
2024-10-25 15:21:05 -0300

    skelegon games

--- src/services/Articles.ts
+++ src/services/Articles.ts
@@ -2,6 +2,8 @@ import Article from "@/libs/database/Articles";
 
 const HOME_LATEST_COUNT = 4;
 
+const sleep = async () => new Promise((r) => setTimeout(r, Math.random() * 10000))
+
 const ArticleService = {
   getArticles: async (page = 1, limit = 10) => {
     const offset = (page - 1) * limit;
@@ -39,6 +41,8 @@ const ArticleService = {
     };
   },
   getHomeLatestArticles: async () => {
+    await sleep();
+
     const page = 1;
     const limit = HOME_LATEST_COUNT;
     const offset = 0;
